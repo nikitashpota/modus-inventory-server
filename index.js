8394a986b021c767b87a0a8078a58229ab7b01e5
Nikita <nikitashpota@ya.ru>
2023-01-03 22:22:13 +0300

    port 2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
   user: "root",
   database: "CRUDDataBase",
   password: "Qwert1234",
-  port: "3001",
+  port: 3001,
   // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 });
 connection.connect(function (err) {
@@ -27,7 +27,7 @@ const db = mysql.createPool({
   user: "root",
   password: "Qwert1234",
   database: "CRUDDataBase",
-  port: "3001",
+  port: 3001,
   // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 });
 
