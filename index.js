77faaf56a2e1226d58d0a00eff57401da369782a
Nikita <nikitashpota@ya.ru>
2023-01-03 21:34:13 +0300

    log

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "'root'@'localhost'",
   database: "CRUDDataBase",
   password: "Qwert1234",
 });
